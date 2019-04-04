6df5c8fb994b557cd97dd189fce9fc3c3a86114c
hiroki suganuma <da78245@qa3.so-net.ne.jp>
2019-04-05 08:36:01 +0900

    モック用ルート作成（質問用）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   get 'users/new'
+  get 'questions/new'
+  get 'questions/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

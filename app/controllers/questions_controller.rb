ba468cac55e06f80a34b5bb593af9967d989aef8
hiroki suganuma <da78245@qa3.so-net.ne.jp>
2019-05-15 22:54:08 +0900

    質問投稿機能（仮作成）

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,25 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
-  def def new
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @user = User.find(1)
+    @question = @user.questions.build(question_params)
+    if @question.save
+      flash[:success] = "質問が完了しました。"
+      #redirect_to user_path
+    else
+      redirect_to questions_new_path
+    end
   end
   
+private
+
+  def question_params
+    params.require(:question).permit(:title, :content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'questions/new'
   get 'questions/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :questions
 end
